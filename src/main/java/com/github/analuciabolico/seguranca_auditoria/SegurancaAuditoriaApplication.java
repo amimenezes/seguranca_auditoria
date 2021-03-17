831a4097f7ef4a81ab170fb035665ca7fc18b065
analuciabolico <analuciabolico.dev@gmail.com>
2021-03-17 04:01:02 -0300

    first commit

---
+++ src/main/java/com/github/analuciabolico/seguranca_auditoria/SegurancaAuditoriaApplication.java
@@ -0,0 +1,13 @@
+package com.github.analuciabolico.seguranca_auditoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancaAuditoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegurancaAuditoriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/analuciabolico/seguranca_auditoria/SegurancaAuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.analuciabolico.seguranca_auditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurancaAuditoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
